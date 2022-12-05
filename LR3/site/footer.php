3646a7669b9968c18b7ed30f5c403c16c1e7c3b1
Vellyant <87531124+Vellyant@users.noreply.github.com>
2022-12-05 15:07:57 +0300

    modified:   LR2/site/.core/companyTable.php 	modified:   LR2/site/.core/db.php 	modified:   LR2/site/.core/regionTable.php 	modified:   LR2/site/enterprises.php 	new file:   LR3/database structure.png 	new file:   LR3/dump_list_enterprises.sql 	new file:   LR3/index.php 	new file:   LR3/site/.core/companyTable.php 	new file:   LR3/site/.core/db.php 	new file:   LR3/site/.core/index.php 	new file:   LR3/site/.core/regionTable.php 	new file:   LR3/site/.core/setting.ini 	new file:   LR3/site/css/bootstrap.css 	new file:   LR3/site/css/style.css 	new file:   LR3/site/enterprises.php 	new file:   LR3/site/footer.php 	new file:   LR3/site/header.php 	new file:   LR3/site/inc/img/card-carusel.jfif 	new file:   LR3/site/inc/img/card-filling-machine.jpg 	new file:   LR3/site/inc/img/card-pump.jfif 	new file:   LR3/site/inc/img/card-tanks.jpg 	new file:   LR3/site/inc/img/equipment_1.jfif 	new file:   LR3/site/inc/img/equipment_2.jfif 	new file:   LR3/site/inc/img/equipment_3.jfif 	new file:   LR3/site/inc/img/equipment_4.jfif 	new file:   LR3/site/inc/img/i1.svg 	new file:   LR3/site/inc/img/i4.svg 	new file:   LR3/site/inc/img/logo-agro.jpeg 	new file:   LR3/site/inc/img/logo.png 	new file:   LR3/site/inc/img/scheme.jfif 	new file:   LR3/site/inc/logo_enterprises/PK_MKL_.png 	new file:   LR3/site/inc/logo_enterprises/SVV_group.png 	new file:   LR3/site/inc/logo_enterprises/avangard.png 	new file:   LR3/site/inc/logo_enterprises/avanta_group.jpg 	new file:   LR3/site/inc/logo_enterprises/china_tech.jpg 	new file:   LR3/site/inc/logo_enterprises/cilco.png 	new file:   LR3/site/inc/logo_enterprises/global_74.png 	new file:   LR3/site/inc/logo_enterprises/izmbt.jpg 	new file:   LR3/site/inc/logo_enterprises/no_image.png 	new file:   LR3/site/inc/logo_enterprises/pamir.png 	new file:   LR3/site/inc/logo_enterprises/riatom.png 	new file:   LR3/site/inc/logo_enterprises/royal.png 	new file:   LR3/site/inc/logo_enterprises/skb_4.png 	new file:   LR3/site/inc/logo_enterprises/stm.jpg 	new file:   LR3/site/inc/logo_enterprises/technotrend.jpg 	new file:   LR3/site/inc/logo_enterprises/transsertico.png 	new file:   LR3/site/inc/logo_enterprises/uranus.png 	new file:   LR3/site/index.php 	new file:   LR3/site/js/bootstrap.bundle.js 	new file:   LR3/site/js/bootstrap.js 	modified:   README.md

---
+++ LR3/site/footer.php
@@ -0,0 +1,120 @@
+<!-- FOOTER  подвал -->
+<footer class="bg-blue-navbar">
+      <div class="p-3 container">
+
+        <div class="d-flex flex-row footer__navigation">
+          <a class="p-2 link-light text-decoration-none" href="#">УПРАВЛЕНИЕ ПРЕДПРИЯТИЕМ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">ПОЛИТИКА ПОРТАЛА </a>
+          <a class="p-2 link-light text-decoration-none" href="#">ВЫСТАВКИ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">О ПРОЕКТЕ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">НОВОСТИ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">КОМПАНИЙ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">ОБЗОР РЫНКОВ </a>
+          <a class="p-2 link-light text-decoration-none" href="#">ЛИЗИНГ</a>
+        </div>
+        <hr class="text-light">
+
+        <!-- Скрытый блок скрывающийся блок-->
+        <input class="hide" id="hd-1" type="checkbox">
+        <label for="hd-1">Продажа оборудования</label>
+        <div>
+          <!-- Скрытое содержание-->
+          <div class="row">
+            <ul class="col-sm-2 list-unstyled">
+              <li><a class="link-light city-link" href="#">Нижний Новгород</a></li>
+              <li><a class="link-light city-link" href="#">Пермь</a></li>
+              <li><a class="link-light city-link" href="#">Уфа</a> </li>
+              <li><a class="link-light city-link" href="#">Казань</a></li>
+            </ul>
+            <ul class="col-sm-2 list-unstyled">
+              <li><a class="link-light city-link" href="#">Ижевск</a></li>
+              <li><a class="link-light city-link" href="#">Самара</a></li>
+              <li><a class="link-light city-link" href="#">Санкт-Петербург</a></li>
+              <li><a class="link-light city-link" href="#">Красноярск</a></li>
+
+            </ul>
+            <ul class="col-sm-2 list-unstyled">
+              <li><a class="link-light city-link" href="#">Новосибирск</a></li>
+              <li><a class="link-light city-link" href="#">Омск</a></li>
+              <li><a class="link-light city-link" href="#">Екатеринбург</a></li>
+              <li><a class="link-light city-link" href="#">Челябинск</a></li>
+            </ul>
+            <ul class="col-sm-2 list-unstyled">
+              <li> <a class="link-light city-link" href="#">Владимир</a></li>
+              <li> <a class="link-light city-link" href="#">Воронеж</a></li>
+              <li> <a class="link-light city-link" href="#">Калуга</a></li>
+              <li> <a class="link-light city-link" href="#">Кострома</a></li>
+            </ul>
+            <ul class="col-sm-2 list-unstyled">
+              <li> <a class="link-light city-link" href="#">Москва</a></li>
+              <li> <a class="link-light city-link" href="#">Тверь</a></li>
+              <li> <a class="link-light city-link" href="#">Тула</a></li>
+              <li> <a class="link-light city-link" href="#">Ярославль</a></li>
+            </ul>
+            <ul class="col-sm-2 list-unstyled">
+              <li> <a class="link-light city-link" href="#">Волгоград</a></li>
+              <li> <a class="link-light city-link" href="#">Краснодар</a></li>
+              <li> <a class="link-light city-link" href="#">Ростов-на-Дону</a></li>
+            </ul>
+
+          </div>
+
+        </div>
+        <!-- конец Скрытый блок скрывающийся блок-->
+      </div>
+
+
+
+      <div class="bg-footer2">
+
+        <div class="p-3 container">
+          <div class="text-light row small">
+            <div class="col-md-3">
+              2000 — 2022 © «Гипер-Пресс»<br>
+              Equipnet.ru — "Оборудование для бизнеса"
+            </div>
+            <div class="col-md-4">
+              Все материалы данного сайта, авторские права на которые принадлежат EquipNet.ru, не могут быть
+              скопированы.
+            </div>
+            <div class="col-md-3">
+              Реклама: <a class="link-light city-link" href="#">reklama@equipnet.ru</a><br>
+              Тех. поддержка: <a class="link-light city-link" href="#">support@equipnet.ru</a><br>
+              Редактор сайта: <a class="link-light city-link" href="#">redaktor@equipnet.ru</a><br>
+            </div>
+
+            <div class="col-md-2">
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 172 172"
+                style=" fill:#000000;">
+                <g transform="">
+                  <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                    stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                    font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                    style="mix-blend-mode: normal">
+                    <path d="M0,172v-172h172v172z" fill="none"></path>
+                    <g fill="#ffffff">
+                      <path
+                        d="M163.98408,126.1405c-6.43925,-11.5885 -23.0265,-25.51333 -29.87425,-31.57275c-1.87408,-1.65908 -2.07475,-4.52933 -0.36908,-6.364c13.06842,-14.04308 23.521,-30.17883 27.11867,-39.92908c1.58742,-4.30717 -1.6985,-8.858 -6.32817,-8.858h-13.44825c-4.43258,0 -7.02692,1.591 -8.26317,4.12442c-10.86108,22.25608 -20.17775,31.88808 -26.69942,37.5175c-3.65142,3.15333 -9.34533,0.54108 -9.34533,-4.257c0,-9.245 0,-21.19542 0,-29.67c0,-4.11008 -3.36117,-7.43542 -7.50708,-7.43542l-24.56733,-0.2795c-3.09242,0 -4.859,3.50092 -3.00283,5.9555l4.05633,5.82292c1.5265,2.01742 2.35067,4.472 2.35067,6.99108l-0.0215,26.22283c0,4.56158 -5.52908,6.79042 -8.81142,3.59408c-11.10475,-10.8145 -20.74033,-32.89858 -24.209,-43.45867c-1.00333,-3.05658 -3.86642,-5.12058 -7.11292,-5.12775l-13.24758,-0.03225c-4.97008,0 -8.60358,4.71208 -7.25267,9.45642c12.1045,42.48758 36.94058,82.90758 81.20192,87.29c4.36092,0.43 8.12342,-3.08167 8.12342,-7.42825v-13.7815c0,-3.95242 3.09958,-7.34942 7.08425,-7.44975c0.13975,-0.00358 0.2795,-0.00358 0.41925,-0.00358c11.70675,0 24.81817,17.03875 29.40483,25.00808c1.31867,2.29333 3.784,3.69083 6.45,3.69083h17.81992c5.19942,0 8.5355,-5.51475 6.03075,-10.02617z">
+                      </path>
+                    </g>
+                    <path d="" fill="none"></path>
+                  </g>
+                </g>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-youtube"
+                viewBox="0 0 16 16">
+                <path
+                  d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
+              </svg>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+    <!-- END FOOTER конец подвал -->
+
+    </body>
+
+</html>
