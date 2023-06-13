db39c86fb4e1f6a5fc86501712dbe861ebff1907
Vellyant <87531124+Vellyant@users.noreply.github.com>
2023-06-13 20:45:37 +0300

    deleted:    vkr/1 	deleted:    vkr/index.php 	deleted:    vkr/site/.core/action.php 	deleted:    vkr/site/.core/companyTable.php 	deleted:    vkr/site/.core/db.php 	deleted:    vkr/site/.core/feed.php 	deleted:    vkr/site/.core/index.php 	deleted:    vkr/site/.core/productTable.php 	deleted:    vkr/site/.core/setting.ini 	deleted:    vkr/site/.core/userAction.php 	deleted:    vkr/site/.core/userLogic.php 	deleted:    vkr/site/.core/userTable.php 	deleted:    vkr/site/company.php 	deleted:    vkr/site/create_feed.php 	deleted:    vkr/site/create_list_product.php 	deleted:    vkr/site/css/bootstrap.css 	deleted:    vkr/site/css/style.css 	deleted:    vkr/site/footer.php 	deleted:    vkr/site/header.php 	deleted:    vkr/site/inc/feed/1/fed.yml 	deleted:    vkr/site/inc/feed/1/ffs.yml 	deleted:    vkr/site/inc/feed/testfile.txt 	deleted:    vkr/site/js/bootstrap.bundle.js 	deleted:    vkr/site/js/bootstrap.js 	deleted:    vkr/site/list_feed.php 	deleted:    vkr/site/list_products.php 	deleted:    vkr/site/product.php 	deleted:    vkr/site/sign_in.php 	deleted:    vkr/site/sign_up.php 	deleted:    vkr/site/start_page.php

--- vkr/site/create_list_product.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/site/.core/index.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . "/site/header.php");
-
-
-$list_pr = product::get_list_products();
-?>
